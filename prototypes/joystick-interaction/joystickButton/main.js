20eb8600f569ddb596fb5be0253caf3353e66782
Felix Blomqvist <felixblomqvist19@gmail.com>
2019-03-13 09:46:24 +0100

    no message

--- prototypes/joystick-interaction/joystickButton/main.js
+++ prototypes/joystick-interaction/joystickButton/main.js
@@ -11,22 +11,22 @@ let button4 = document.getElementById("coldestColour");
 
 button1.addEventListener("mouseover", function(event) {
         //Fill background
-        document.body.style.backgroundColor = "red";
+        document.body.style.backgroundColor = "#401201";
 });
 
 button2.addEventListener("mouseover", function(event) {
     //Fill background
-    document.body.style.backgroundColor = "blue";
+    document.body.style.backgroundColor = "#73473D";
 });
 
 button3.addEventListener("mouseover", function(event) {
     //Fill background
-    document.body.style.backgroundColor = "green";
+    document.body.style.backgroundColor = "#7B9BA6";
 });
 
 button4.addEventListener("mouseover", function(event) {
     //Fill background
-    document.body.style.backgroundColor = "pink";
+    document.body.style.backgroundColor = "#EEF4F2";
 });
 
 
