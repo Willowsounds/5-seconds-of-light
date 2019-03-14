bcadc12feef670ec663c5972e06ec9063fb4521d
Felix Blomqvist <felixblomqvist19@gmail.com>
2019-03-14 13:29:57 +0100

    no message

--- prototypes/joystick-interaction/joystickButton/main.js
+++ prototypes/joystick-interaction/joystickButton/main.js
@@ -2,21 +2,22 @@
 //mouse variables
 let mouseX;
 let mouseY;
-
-let mouseClick;
 let isMouseOver;
+
+//button variables
 let button1 = document.getElementById("warmestColour");
 let button2 = document.getElementById("warmColour");
 let button3 = document.getElementById("coldColour");
 let button4 = document.getElementById("coldestColour");
 
-/*Colors:
+/*Colors that are used:
 Warmest: "#401201"
 Warm: "#73473D"
 Cold: "#7B9BA6"
 Coldest: "#EEF4F2"
 */
 
+// Event listeners for all of the different buttons
 button1.addEventListener("mouseover", function(event) {
       //Fill background
       document.body.style.backgroundColor = "#401201";
@@ -37,26 +38,6 @@ button4.addEventListener("mouseover", function(event) {
     document.body.style.backgroundColor = "#EEF4F2";
 });
 
-button1.addEventListener("mouseclick", function(event) {
-    //Fill background
-    document.body.style.backgroundColor = "#401201";
-    console.log("button1 clicked!");
-});
-
-button2.addEventListener("mouseclick", function(event) {
-    //Fill background
-    document.body.style.backgroundColor = "#73473D";
-});
-
-button3.addEventListener("mouseclick", function(event) {
-    //Fill background
-    document.body.style.backgroundColor = "#7B9BA6";
-});
-
-button4.addEventListener("mouseclick", function(event) {
-    //Fill background
-    document.body.style.backgroundColor = "#EEF4F2";
-});
 
 //handle mouse elements
 function handleMouseMove(event){
@@ -64,14 +45,11 @@ function handleMouseMove(event){
     mouseY = event.pageY;
 }
 
-
+// If the mouse is over one of the divs, this function will be called
 function handleMouseOver(event){
     isMouseOver = false;
 }
 
-function handleMouseClick(event){
-  mouseClick = false;
-}
 
 //set listeners
 document.addEventListener("mouseover", handleMouseOver, false);
