c3c7d1d2e59e6998f81873c625af23b9a891c413
confident1 <47142559+confident1@users.noreply.github.com>
2019-03-25 22:59:22 +0100

    Update main.js

--- first-iteration/prototypes/joystick-interaction/joystickButton/main.js
+++ first-iteration/prototypes/joystick-interaction/joystickButton/main.js
@@ -20,22 +20,22 @@ Coldest: "#EEF4F2"
 // Event listeners for all of the different buttons
 button1.addEventListener("mouseover", function(event) {
       //Fill background
-      document.body.style.backgroundColor = "#401201";
+      document.body.style.backgroundColor = "hsl(16.2, 96.9%, 12.7%)";
 });
 
 button2.addEventListener("mouseover", function(event) {
     //Fill background
-    document.body.style.backgroundColor = "#73473D";
+    document.body.style.backgroundColor = "hsl(11.1, 30.7%, 34.5%)";
 });
 
 button3.addEventListener("mouseover", function(event) {
     //Fill background
-    document.body.style.backgroundColor = "#7B9BA6";
+    document.body.style.backgroundColor = "hsl(195.3, 19.5%, 56.7%)";
 });
 
 button4.addEventListener("mouseover", function(event) {
     //Fill background
-    document.body.style.backgroundColor = "#EEF4F2";
+    document.body.style.backgroundColor = "hsl(160, 21.4%, 94.5%)";
 });
 
 
