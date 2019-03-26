a3c5e5ba64b6895a1a5f846ec8aad2365650e2fe
Patrik Svensson <a.patrik.svensson@gmail.com>
2019-03-26 09:05:39 +0100

    Initial commit of arduino code and example.

---
+++ first iteration/ultrasound/README.md
@@ -0,0 +1,3 @@
+This code is based on code written by Jontas (http://www.f15ijp.com/2012/09/arduino-ultrasonic-sensor-hc-sr04-or-hy-srf05/)
+
+
---
+++ first iteration/ultrasound/arduino/arduino.ino
@@ -0,0 +1,163 @@
+// Serial I/O based on example http://forum.arduino.cc/index.php?topic=396450
+// Ultra sound sensor code based on 
+// example http://www.f15ijp.com/2012/09/arduino-ultrasonic-sensor-hc-sr04-or-hy-srf05/
+// and https://create.arduino.cc/projecthub/rztronics/ultrasonic-range-detector-using-arduino-and-sr-04f-8a804d
+
+
+
+// Serial communication
+const byte numChars = 32;
+char receivedChars[numChars];
+char tempChars[numChars];
+char messageFromPC[numChars] = {0};
+int integerFromPC = 0;
+float floatFromPC = 0.0;
+boolean newData = false;
+long lastReport = 0;
+
+const int TRIG_PIN = 12;
+const int ECHO_PIN = 13;
+int analogPinA0 = A0;
+int sensorValue = 0;
+
+enum {
+  MsgAcknowledge, // 0
+  MsgError,       // 1
+  MsgMove,        // 2
+  MsgMoveResult,  // 3
+  MsgPosition,    // 4
+};
+
+
+void setup()  {
+ Serial.begin(115200);
+ //pinMode( buttonPinA, INPUT);
+ //pinMode( buttonPinB, INPUT);
+ //pinMode( buttonPinC, INPUT);
+ //pinMode( buttonPinD, INPUT);
+ //pinMode( analogPinA0, INPUT);
+ pinMode(TRIG_PIN,OUTPUT);
+ pinMode(ECHO_PIN,INPUT);
+ report(MsgAcknowledge, "Ready");
+}
+
+void loop() {
+  // Process serial communucation
+  recvWithStartEndMarkers();
+
+  // If we received a command, process it
+  if (newData == true) {
+    strcpy(tempChars, receivedChars);
+    parseData();
+    switch (integerFromPC) {
+      case MsgPosition:
+        // Report fake result
+        report(MsgPosition, 10);
+        break;
+      case MsgMove:
+        // Pretend to do something
+        report(MsgMoveResult, 10);
+        break;
+      }
+
+      // Debug: print parsed command to serial
+      // showParsedData();
+      newData = false;
+    }
+
+
+  // Every so often report a fake position
+  if (millis() - lastReport > 500) {
+    long duration, distanceCm, distanceIn;
+ 
+    // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+    duration = pulseIn(ECHO_PIN,HIGH);
+ 
+    // convert the time into a distance
+    distanceCm = duration / 29.1 / 2 ;  
+    
+    report(MsgPosition, distanceCm);
+    lastReport = millis();
+
+  }
+}
+
+// ---- Serial communication
+void report(int code, const char *message) {
+  Serial.print("<");
+  Serial.print("ws-bridge,");
+  Serial.write(code);
+  Serial.write(",");
+  Serial.write(message);
+  Serial.print(">\r\n");
+  Serial.flush();
+}
+
+void report(int code, int message) {
+  Serial.print("<");
+  Serial.print("ws-bridge,");
+  Serial.print(code);
+  Serial.print(",");
+  Serial.print(message);
+  Serial.print(">\r\n");
+  Serial.flush();
+}
+
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+void parseData() {      // split the data into its parts
+    char * strtokIndx; // this is used by strtok() as an index
+
+    strtokIndx = strtok(tempChars,",");      // get the first part - the string
+    strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
+
+    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+    integerFromPC = atoi(strtokIndx);     // convert this part to an integer
+
+    strtokIndx = strtok(NULL, ",");
+    floatFromPC = atof(strtokIndx);     // convert this part to a float
+}
+
+void showParsedData() {
+    Serial.print("Message ");
+    Serial.println(messageFromPC);
+    Serial.print("Integer ");
+    Serial.println(integerFromPC);
+    Serial.print("Float ");
+    Serial.println(floatFromPC);
+}
